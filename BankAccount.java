70dbf3cbdd4bbc9b8e55c802bace757b46c43d37
Maftum Hasnain <maftumhasnain@gmail.com>
2020-05-05 00:38:55 +0600

    first commit

---
+++ BankAccount.java
@@ -0,0 +1,127 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class BankAccount
+{
+	int balance;
+	int previousTransaction;
+	String customerName;
+	String customerID;
+	
+	BankAccount(String cname,String cid)
+	{
+		customerName = cname;
+		customerID = cid;
+	}
+	
+	void deposite(int amount)
+	{
+		if(amount !=0)
+		{
+			balance = balance + amount;
+			previousTransaction = amount;
+		}
+	}
+	
+	void withdraw(int amount)
+	{
+		if(amount != 0)
+		{
+			balance = balance - amount;
+			previousTransaction = -amount;
+		}
+	}
+	
+	void getPreviousTransaction()
+	{
+		if(previousTransaction > 0)
+		{
+			System.out.println("Deposited: "+previousTransaction);
+		}
+		else if(previousTransaction < 0)
+		{
+			System.out.println("withdraw: "+Math.abs(previousTransaction));
+	    }
+		else
+		{
+			System.out.println("No transaction occured");
+        }
+	}
+	
+	void showMenu()
+	{
+		
+		char option='\0';
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("");
+		System.out.println("Welcome to our service Mr. "+customerName);
+		System.out.println("Your ID is "+customerID);
+		System.out.println("");
+		System.out.println("A. Check Balance:-");
+		System.out.println("B. Deposit:-");
+		System.out.println("C. Withdraw:-");
+		System.out.println("D. PreviousTransaction:-");
+		System.out.println("E. Exit:-");
+		
+		do 
+		{
+			System.out.println("********************");
+			System.out.println("Enter an option :-");
+			System.out.println("********************");
+			option = scanner.next().charAt(0);
+			System.out.println("\n");
+			
+			switch(option)
+			{
+				
+				case 'A':
+				System.out.println("****************************");
+				System.out.println("Balance = "+balance);
+				System.out.println("****************************");
+				System.out.println("\n");
+				break;
+				
+				case 'B':
+				System.out.println("****************************");
+				System.out.println("Enter an Amount to Deposit:");
+				System.out.println("****************************");
+				System.out.print("Amount: ");
+				int amount = scanner.nextInt();
+				deposite(amount);
+				System.out.println("\n");
+				break;
+				
+				case 'C':
+				System.out.println("****************************");
+				System.out.println("Enter an Amount to Withdraw:");
+				System.out.println("****************************");
+				System.out.print("Amount: ");
+				int amount2 = scanner.nextInt();
+				withdraw(amount2);
+				System.out.println("\n");
+				break;
+				
+				case 'D':
+				System.out.println("****************************");
+				getPreviousTransaction();
+				System.out.println("****************************");
+				System.out.println("\n");
+				break;
+				
+				case 'E':
+			    System.out.println("************************************************");
+				break;
+				
+				default:
+				System.out.println("Invalid OPtion!!.Please enter again");
+				break;
+				
+			}
+		}
+		while(option != 'E');
+		
+		System.out.println("Thank you for using our services  :)");
+		System.out.println("************************************************");
+	}
+}
---
+++ BankingApplication.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.lang.*;
+
+
+public class BankingApplication
+{
+	public static void main(String[] args)
+	
+	{
+	 	BankAccount b1 = new BankAccount("Maftum Hasnain","Z25XC39");
+		b1.showMenu();
+	}
+	
+}
+
+
